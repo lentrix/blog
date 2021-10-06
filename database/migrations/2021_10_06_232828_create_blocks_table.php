f82e12c8456cf34204792ce2e91ba92b8a7c1446
lentrix <hawkmanlentrix@gmail.com>
2021-10-07 07:37:47 +0800

    first commit

---
+++ database/migrations/2021_10_06_232828_create_blocks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blocks', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('post_id')->unsigned();
+            $table->text('content');
+            $table->integer('order')->default(0);
+            $table->timestamps();
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blocks');
+    }
+}
